ad90de561d6dd03657948cac392c3a0ddc13aa28
maekawa <maekawa@vagrantserver.(none)>
2014-09-28 12:12:55 +0900

    first commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'jenkins'
+maintainer       'hidetoshi maekawa'
+maintainer_email 'hidetoshi_maekawa@e-it.co.jp'
+license          'All rights reserved'
+description      'Installs/Configures jenkins'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
